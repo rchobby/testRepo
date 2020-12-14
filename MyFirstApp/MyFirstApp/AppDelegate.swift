92646c446a1f98694d8b575ec55392fbdaa98488
rchobby <rchobby@gmail.com>
2020-12-14 13:48:32 +0700

    Newly Created Project

---
+++ MyFirstApp/MyFirstApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyFirstApp
+//
+//  Created by iSon on 14/12/2563 BE.
+//  Copyright © 2563 iSon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyFirstApp/MyFirstApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by iSon on 14/12/2563 BE.
+//  Copyright © 2563 iSon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
